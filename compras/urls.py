e3536fb5a26436fe4cfe8f3af293b5eab59fafc9
Alejandro347 <alemonsan05@gmail.com>
2025-03-27 14:42:41 -0600

    cambios

--- compras/urls.py
+++ compras/urls.py
@@ -2,9 +2,9 @@ from django.urls import include, path
 from rest_framework import routers
 from .views import ListaCompraViewSet
 
-router = routers.DefaultRouter()
-router.register(r'listaCompra', ListaCompraViewSet)
+rutas = routers.DefaultRouter()
+rutas.register(r'listaCompra', ListaCompraViewSet)
 
 urlpatterns = [
-    path('', include(router.urls))
+    path('', include(rutas.urls))
 ]
